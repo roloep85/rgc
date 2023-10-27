0887eb75ade238b598fc2057eecc85e32254b737
rologit85 <roloep@live.com>
2023-10-27 14:47:31 +0200

    disclaimer page animation

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -1,6 +1,13 @@
+"use client";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const DisclaimerPage = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0,
+  });
+
   return (
     <>
       <div
@@ -17,9 +24,27 @@ const DisclaimerPage = () => {
           </div>
         </div>
       </div>
-      <div className='container px-2 sm:px-20 mx-auto py-40'>
+      <div ref={ref} className='container px-2 sm:px-20 mx-auto py-40'>
         <div className='max-w-lg mx-auto text-justify'>
-          <p className='mb-5'>
+          <p
+            className={`mb-5 transition-transform duration-700 transform-gpu ${
+              inView ? "translate-y-0" : "translate-y-96"
+            }`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+            numquam architecto id facere quia beatae rem nesciunt quisquam
+            laudantium explicabo consequatur ex cupiditate, accusantium impedit
+            repudiandae recusandae saepe blanditiis, quibusdam at ullam! Autem,
+            rem fugit? Dolorum velit eaque, consequuntur ad natus iure dolor ab
+            corporis explicabo, eius praesentium! Exercitationem corrupti
+            quibusdam doloremque sed sint quod repellat dignissimos iusto
+            debitis culpa distinctio voluptate facere, quo laborum.
+          </p>
+          <p
+            className={`mb-5 transition-transform duration-700 delay-200 transform-gpu ${
+              inView ? "translate-y-0" : "translate-y-96"
+            }`}
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
             numquam architecto id facere quia beatae rem nesciunt quisquam
             laudantium explicabo consequatur ex cupiditate, accusantium impedit
